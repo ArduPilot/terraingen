0f1995fb9ace21cb015f0ffb47458f39186edad1
Stephen Dade <stephen_dade@hotmail.com>
2021-08-23 21:28:05 +1000

    Add generation of ArduPilot 4.1+ terrain

--- offline_gen.py
+++ offline_gen.py
@@ -16,17 +16,20 @@ import shutil
 from MAVProxy.modules.mavproxy_map import srtm
 from terrain_gen import create_degree
 
-def worker(downloader, lat, long, targetFolder, startedTiles, totTiles):
+def worker(downloader, lat, long, targetFolder, startedTiles, totTiles, format):
     # only create if the output file does not exists
     if os.path.exists(datafile(lat, long, targetFolder) + '.gz'):
-        print("Skipping existing compressed tile {0} of {1} ({2:.3f}%)".format(startedTiles, totTiles, ((startedTiles)/totTiles)*100))
+        print("Skipping existing compressed tile {0} of {1} ({2:.3f}%)".format(startedTiles+1, totTiles, ((startedTiles)/totTiles)*100))
         return
 
     if not os.path.exists(datafile(lat, long, targetFolder)):
-        create_degree(downloader, lat, long, targetFolder, 100)
+        if not create_degree(downloader, lat, long, targetFolder, 100, format):
+            #print("Skipping not downloaded tile {0} of {1} ({2:.3f}%)".format(startedTiles+1, totTiles, ((startedTiles)/totTiles)*100))
+            time.sleep(0.2)
+            return
         print("Created tile {0} of {1}".format(startedTiles, totTiles))
     else:
-        print("Skipping existing tile {0} of {1} ({2:.3f}%)".format(startedTiles, totTiles, ((startedTiles)/totTiles)*100))
+        print("Skipping existing tile {0} of {1} ({2:.3f}%)".format(startedTiles+1, totTiles, ((startedTiles)/totTiles)*100))
 
     # and compress
     with open(datafile(lat, long, targetFolder), 'rb') as f_in:
@@ -34,7 +37,7 @@ def worker(downloader, lat, long, targetFolder, startedTiles, totTiles):
             shutil.copyfileobj(f_in, f_out)
     os.remove(datafile(lat, long, targetFolder))
 
-    print("Done tile {0} of {1} ({2:.3f}%)".format(startedTiles, totTiles, ((startedTiles)/totTiles)*100))
+    print("Done tile {0} of {1} ({2:.3f}%)".format(startedTiles+1, totTiles, ((startedTiles+1)/totTiles)*100))
     print("Folder is {0:.0f}Mb in size".format(get_size(targetFolder)/(1024*1024)))
 
 def datafile(lat, lon, folder):
@@ -67,6 +70,9 @@ def get_size(start_path = '.'):
 
     return total_size
 
+def error_handler(e):
+    print('Worker exception: {0}'.format(exc))
+    
 if __name__ == '__main__':
     global filelistDownloadActive
     # Create the parser
@@ -75,15 +81,16 @@ if __name__ == '__main__':
     # Add the arguments
     # Folder to store processed DAT files
     parser.add_argument('-folder', action="store", dest="folder", default="processedTerrain")
+    # Folder to store processed DAT files
+    parser.add_argument('--offline', action="store_true", dest="offline")
     # Number of threads to use
     parser.add_argument('-processes', action="store", dest="processes", type=int, default=4)
     # Latitude range
     parser.add_argument('-latitude', action="store", dest="latitude", type=int, default=60)
-    
-    args = parser.parse_args()
+    # File format
+    parser.add_argument("--format", type=str, default="4.1", choices=["pre-4.1", "4.1"], help="Ardupilot version")
 
-    downloader = srtm.SRTMDownloader(debug=False, cachedir='srtmcache')
-    downloader.loadFileList()
+    args = parser.parse_args()
 
     targetFolder = os.path.join(os.getcwd(), args.folder)
     #create folder if required
@@ -97,6 +104,9 @@ if __name__ == '__main__':
     # store the threads
     processes = []
 
+    downloader = srtm.SRTMDownloader(debug=False, offline=(1 if args.offline else 0))
+    downloader.loadFileList()
+    
     # make tileID's
     tileID = []
     i = 0
@@ -111,7 +121,7 @@ if __name__ == '__main__':
 
     # Use a pool of workers to process
     with ThreadPool(args.processes-1) as p:
-        reslist = [p.apply_async(worker, args=(downloader, td[0], td[1], targetFolder, td[2], len(tileID))) for td in tileID]
+        reslist = [p.apply_async(worker, args=(downloader, td[0], td[1], targetFolder, td[2], len(tileID), args.format), error_callback=error_handler) for td in tileID]
         for result in reslist:
             result.get()
 
